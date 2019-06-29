8fc3dc44e2299d0ac82047d44a2736b769c9363a
shyamuu1 <35182869+shyamuu1@users.noreply.github.com>
2019-06-29 15:01:21 -0400

    Add files via upload
    Fixed CarLot and implements functions in menus

--- CarLot.java
+++ CarLot.java
@@ -1,5 +1,54 @@
 package project0;
 
-public class CarLot {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
+public class CarLot extends Car {
+
+
+	public CarLot() {
+		super();
+	}
+
+	public static List<Car> cars = new ArrayList<>();
+
+	static void addCar(Car c) { 
+		cars.add(c);
+
+	}
+
+	static void removeCar(Car c) {
+		cars.remove(c);
+
+	}
+
+	public static void ViewCar(List<Car> c) {
+		Iterator<Car> it = cars.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+
+		}
+		
+
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		return true;
+	}
+
+	
 }
--- Menu.java
+++ Menu.java
@@ -1,36 +1,50 @@
 package project0;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
+	private static Scanner sc;// to not write sc everytime
+
+	public static List<Car> cars = new ArrayList<>();
 
 	static void mainMenu() {
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 		// print menu
-		System.out.println("Main Menu\n\n1)Customer Login\n2)Employee Login\n3)Cars\n4)Exit\n");
+		System.out.println("Main Menu\n\n1)Customer Login\n2)Employee Login\n3)Cars\n4)Register\n5)exit\n");
 		//
+		//List<Car> cars = new ArrayList<>();
 
 		boolean quit = false;
 
 		int menuItem;
 		do {
+			while (!sc.hasNextInt()) {
+				System.out.println("Type in an integer");
+				sc.next();
+			}
 
 			menuItem = sc.nextInt();
 
 			switch (menuItem) {
-			case 1:
-				userMenu();
+			case 1:// customer access
+				CuserMenu();
 				// do something;
 				break;
-			case 2:
-				userMenu();
+			case 2:// Employee Access
+				empUserMenu();
 				// do something;
 				break;
-			case 3:
+			case 3:// Cars available on lot
 				System.out.println("cars");
+				CarLot.ViewCar(cars);
 				// do something;
 				break;
-			case 4:
+			case 4: // Register for online account
+				break;
+			case 5:// Exit
 				System.out.println("Quit!");
 				quit = true;
 				break;
@@ -40,13 +54,13 @@ public class Menu {
 		} while (!quit);
 		System.out.println("Adios, amigo..");
 
-		sc.close();
 	}
 
 	static void customerMenu() {
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 		int custoptn;// customer options keys
 		boolean exit = false;
+		
 
 		System.out.println("Customer realm\n\n1)Payments\n2)Cars owned\n3)Offer\n4)Exit");
 		do {
@@ -54,10 +68,12 @@ public class Menu {
 
 			switch (custoptn) {
 			case 1:
-				//payments
+				// payments
+				// cost/ # of months for loan(monthly bill) subtracted from total cost
 				break;
 			case 2:
-				System.out.println("My cars");
+				System.out.println("My cars");// list of cars
+				CarLot.ViewCar(cars);
 				break;
 			case 3:
 				System.out.println("Offer available (Accept or decline)");
@@ -71,33 +87,45 @@ public class Menu {
 			}
 		} while (!exit);
 		System.out.println("Au revoir ami!");
-
-		sc.close();
+		mainMenu();
 
 	}
 
-	static void userMenu() {
-		Scanner sc = new Scanner(System.in);
+	static void CuserMenu() {
+
+		sc = new Scanner(System.in);
 		boolean exit = false;
 		do {
 			System.out.println("Enter UserName: ");// prompt user to input username
-			String username = sc.next();// user types username
+			while (!sc.hasNext()) {
+				System.out.println("Invalid Entry, Try again!");
+				sc.next();
+			}
+			String username = sc.next();
 			System.out.println("Enter Password: ");// prompt user to input pass
-			String pass = sc.next();// user types pass
+			while (!sc.hasNext()) {
+				System.out.println("Invalid Entry");
+				sc.nextLine();
+			} // user types pass
+			String pass = sc.next();
 			System.out.println("Confirm Password: ");
+			while (!sc.hasNext()) {
+				System.out.println("Invalid Entry");
+				sc.nextLine();
+			}
 			String pass1 = sc.next();
 			// Authentication
-			if (pass.contentEquals(pass1)) {
+			if (pass != null && pass.contentEquals(pass1)) {
 				System.out.println("Welcome to Customer Home screen");
-				break;
-			} else if (!pass.contentEquals(pass1)) {
+				customerMenu();
+			} else if (pass == null || !pass.contentEquals(pass1)) {
 				System.out.println("Try again!");
 				System.out.println("Would you like to return back to the main menu? Y/N ");
 				String answer = sc.next();
-				if(answer.contentEquals("Y")) {
+				if (answer.contentEquals("Y")) {
 					break;
-				}else {
-					userMenu();
+				} else {
+					CuserMenu();
 				}
 			}
 		} while (!exit);
@@ -105,13 +133,173 @@ public class Menu {
 		mainMenu();
 	}
 
-	static void employeeMenu() {
-		userMenu();
-		
-	}
+	static void empUserMenu() {
+		sc = new Scanner(System.in);
+		boolean exit = false;
+		do {
 
-	public static void main(String[] args) {
+			System.out.println("Enter Employee Id: ");// prompt user to input username
+			while (!sc.hasNextInt()) {
+				System.out.println("Invalid Entry");
+				sc.next();
+			}
+			int id = sc.nextInt();
+			System.out.println("Enter Password: ");// prompt user to input pass
+			while (!sc.hasNext()) {
+				System.out.println("Invalid Entry");
+				sc.next();
+			} // user types pass
+			String pass = sc.next();
+			System.out.println("Confirm Password: ");
+			while (!sc.hasNext()) {
+				System.out.println("Invalid Entry");
+				sc.next();
+			}
+			String pass1 = sc.next();
+			// Authentication
+			if (pass.contentEquals(pass1)) {
+				System.out.println("Welcome to Customer Home screen");
+				employeeMenu();
+			} else if (!pass.contentEquals(pass1)) {
+				System.out.println("Try again!");
+				System.out.println("Would you like to return back to the main menu? Y/N ");
+				String answer = sc.next();
+				if (answer.contentEquals("Y")) {
+					break;
+				} else {
+					empUserMenu();
+				}
+			}
+		} while (!exit);
+		System.out.println("return back to main menu");
 		mainMenu();
 	}
 
+	static void Inventory() {
+		sc = new Scanner(System.in);
+		for (int i = 1; i > 0; i--) {
+			System.out.println("What is the make of the car you would like to add?");
+			String Make = sc.next();
+			System.out.println("What is the model of that car?");
+			String model = sc.next();
+			System.out.println("What year was this car manufactured?");
+			while (!sc.hasNextInt()) {
+				System.out.println("Invalid Entry!");
+				sc.next();
+			}
+			int yearManufactured = sc.nextInt();
+			System.out.println("What color is the car? ");
+			String color = sc.next();
+			System.out.println("What body style does the car have? ");
+			String chassisType = sc.next();
+			System.out.println("How much does the car cost?");
+			while (!sc.hasNextDouble()) {
+				System.out.println("Invalid Entry!");
+				sc.next();
+			}
+			Double cost = sc.nextDouble();
+			Car c = new Car(Make, model, yearManufactured, color, chassisType, cost);
+			CarLot.addCar(c);
+		}
+		CarLot.ViewCar(cars);
+	}
+
+	static void Remove() {
+		sc = new Scanner(System.in);
+		System.out.println("How many cars would you like to remove?");
+		while (!sc.hasNextInt()) {
+			System.out.println("Invalid Entry! Try Again..");
+			sc.next();
+		}
+
+		for (int i = 0; i < sc.nextInt(); i++) {
+			System.out.println("What is the make of the car you would like to remove?");
+			String Make = sc.next();
+			System.out.println("What is the model of that car?");
+			String model = sc.next();
+			System.out.println("What year was this car manufactured?");
+			while (!sc.hasNextInt()) {
+				System.out.println("Invalid Entry!");
+				sc.next();
+			}
+			int yearManufactured = sc.nextInt();
+			System.out.println("What color is the car? ");
+			String color = sc.next();
+			System.out.println("What body style does the car have? ");
+			String chassisType = sc.next();
+			System.out.println("How much does the car cost?");
+			while (!sc.hasNextDouble()) {
+				System.out.println("Invalid Entry!");
+				sc.next();
+			}
+			Double cost = sc.nextDouble();
+			Car c = new Car(Make, model, yearManufactured, color, chassisType, cost);
+
+			cars.remove(c);
+
+		}
+		CarLot.ViewCar(cars);
+
+	}
+
+	static void employeeMenu() {
+		// Add cars to lots--X
+		// Remove cars from the lot--X
+		// View all payments
+		// Add or reject pending offer on a car
+		sc = new Scanner(System.in);
+		int Emploptn;// customer options keys
+		boolean exit = false;
+	
+
+		System.out.println("Employee Access\n\n1)Add/Remove Cars\n2)Cars\n3)Offer\n4)View all payments\n5)Exit");
+		do {
+			Emploptn = sc.nextInt();
+
+			switch (Emploptn) {
+			case 1:// Add and Remove cars
+				System.out.println("Would you like to add cars or remove cars?<add/remove>");
+				String answer = sc.next();
+				if (answer.contentEquals("add")) {
+					Inventory();
+					break;
+				} else if (answer.contentEquals("remove")) {
+					Remove();
+					break;
+				}
+				employeeMenu();
+				break;
+			case 2:// List of cars
+				System.out.println("My cars");// list of cars
+				CarLot.ViewCar(cars);
+				break;
+			case 3:// Offers available
+				System.out.println("Offer available (Accept or decline)");
+				break;
+			case 4:
+				// View all payments
+				System.out.println("View all payments!");
+
+			case 5:
+				System.out.println("Exit!");
+				exit = true;
+				break;
+			default:
+				System.out.println("U dun F'ed up!");
+			}
+		} while (!exit);
+		System.out.println("Au revoir ami!");
+
+	}
+
+	static int getMenuSelection(Scanner in) {
+		System.out.println("Main Menu\n\n1)Customer Login\n2)Employee Login\n3)Cars\n4)Register\n5)exit\n");
+		
+		return in.nextInt();
+		
+		
+	}
+	
 }
+
+
