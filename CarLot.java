48e4a2521ffdb83d0bc6c09aa48aad05b4b12f0a
shyamuu1 <shyamuu.vasanjee@gmail.com>
2019-06-29 13:13:42 -0400

    Added some field and functions, AddCar, RemoveCar, and ViewCar

--- CarLot.java
+++ CarLot.java
@@ -1,5 +1,29 @@
 package project0;
 
-public class CarLot {
+public class CarLot extends Car {
+
+	public CarLot() {
+		super();
+	}
+
+	public static List<Car> cars = new ArrayList<>();
+
+	static void addCar(Car c) { 
+		cars.add(new Car());
+
+	}
+
+	static void removeCar(Car c) {
+		cars.remove(new Car());
+
+	}
+
+	public static void ViewCar(List<Car> c) {
+		Iterator<Car> it = cars.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+
+		}
+
 
 }
