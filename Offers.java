605627a83686f9cfd47067113bd49440e8cf0c2a
stafinator <51722172+stafinator@users.noreply.github.com>
2019-06-30 19:20:18 -0400

    Add files via upload

--- Offers.java
+++ Offers.java
@@ -1,8 +1,19 @@
 package com.revature.services;
-import com.revature.beans.CarList;
+import java.util.Scanner;
 
 public class Offers {
+		
+	public static boolean makeOffer() {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How much would you like to offer on the vehicle? ");
+		double offer = sc.nextDouble();
+		System.out.println("Your pending offer is: $ " + offer );
+		return true;
+	}
 	
-	
-
+	public static void main(String[] args) {
+		System.out.println(makeOffer());
+		
+	}
 }
