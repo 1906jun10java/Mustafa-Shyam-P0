10b32296a5922ea16f53c255af2431642ad3110f
stafinator <51722172+stafinator@users.noreply.github.com>
2019-07-04 00:25:28 -0400

    Add files via upload

--- Offers.java
+++ Offers.java
@@ -2,18 +2,46 @@ package com.revature.services;
 import java.util.Scanner;
 
 public class Offers {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static double makeOffer() {
 		
-	public static boolean makeOffer() {
-		
-		Scanner sc = new Scanner(System.in);
+		//Scanner sc = new Scanner(System.in);
 		System.out.println("How much would you like to offer on the vehicle? ");
 		double offer = sc.nextDouble();
 		System.out.println("Your pending offer is: $ " + offer );
-		return true;
+		return offer;
+		
+	}
+	
+	public static boolean decisionOffer(double offer) {
+		
+		System.out.println("The offer on the vehicle was "+ offer 
+				+ "\n . Would you like to accept, reject, or counter this offer?"
+				+ "\n Press A to accept offer."
+				+ "\n Press R to rejcect offer." 
+				+ "\n Press O if you wish to make a counter offer.");
+		
+		String decision = sc.next();
+		//while()
+		if(decision.equals("A")) {
+			System.out.println("The offer has been accepted. Thank You !");
+		}else if(decision.equals("R")) {
+			System.out.println("The offer has been declined. Thank You !");
+		}else if (decision.equals("O")) {
+			System.out.println("What is the counter offer you would like to make?");
+			String counter = sc.next();
+			
+			
+		}
+		
+		
+		return false;
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(makeOffer());
-		
+		//System.out.println(makeOffer());
+		decisionOffer(makeOffer());
+		//makeOffer();
 	}
 }
